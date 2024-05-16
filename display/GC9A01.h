a3790a46237dd796c9fcfd6d013d7740aec828dc
Richard Kroesen <kroesen.ri@gmail.com>
2024-05-16 13:01:50 +0200

    Enh not everything was copied correctly from local demo, fixed the errors.

--- display/GC9A01.h
+++ display/GC9A01.h
@@ -50,14 +50,14 @@ extern "C"
  * GLOBAL PROTOTYPES
  **********************/
 
-int GC9A01_init(void);
+int GC9A01_init(const uint8_t display_id);
 void GC9A01_flush_displays(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p);
 void GC9A01_fill(const uint8_t display_id, int32_t x1, int32_t y1, int32_t x2, int32_t y2, lv_color_t color);
 void GC9A01_map(const uint8_t display_id, int32_t x1, int32_t y1, int32_t x2, int32_t y2, lv_color_t *color_p);
 void GC9A01_setRotation(const uint8_t display_id, uint8_t m);
 void GC9A01_fillRect(const uint8_t display_id, int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
 void GC9A01_fillScreen(const uint8_t display_id, uint16_t color);
-uint16_t GC9A01_Color565(const uint8_t display_id, uint8_t r, uint8_t g, uint8_t b);
+uint16_t GC9A01_Color565(uint8_t r, uint8_t g, uint8_t b);
 void GC9A01_invertDisplay(const uint8_t display_id, bool i);
 void GC9A01_drawPixel(const uint8_t display_id, int16_t x, int16_t y, uint16_t color);
 void GC9A01_drawFastHLine(const uint8_t display_id, int16_t x, int16_t y, int16_t w, uint16_t color);
